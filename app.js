961989ee5a83e0399c66f8d90dbc2a0fe8d07859
KaffeMedDanne <danne.katana@gmail.com>
2024-01-12 09:51:00 +0100

    fixed the hamburger-bar code. change style on drop-menu

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const menu = document.querySelector('#mobile-menu')
+const hamburger = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.navbar-menu')
 
-menu.addEventListener('click', function() {
-    menu.classList.toggle("is-active");
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
-});
+})
